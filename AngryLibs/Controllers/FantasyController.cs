d16496d1618fccd27be20078c46100131946b448
Stephen Trewick <stephent818@gmail.com>
2019-10-05 02:46:45 -0700

    fixed form bug for fantasy story

--- AngryLibs/Controllers/FantasyController.cs
+++ AngryLibs/Controllers/FantasyController.cs
@@ -16,7 +16,7 @@ namespace AngryLibs.Controllers
       return View();
     }
     [Route("/EndRHStory")]
-    public ActionResult NewRobinHood(string name1, string noun1, string noun2, string noun3, string noun4, string noun5, string adjective1,string adjective2,string adjective3,string adjective4)
+    public ActionResult NewRobinHood(string name1, string noun1, string noun2, string noun3, string noun4, string noun5, string noun6, string adjective1,string adjective2,string adjective3,string adjective4)
     {
       ChooseLibs newLibs = new ChooseLibs();
       newLibs.Name1 = name1;
@@ -25,6 +25,7 @@ namespace AngryLibs.Controllers
       newLibs.PluralNoun3 = noun3;
       newLibs.PluralNoun4 = noun4;
       newLibs.PluralNoun5 = noun5;
+      newLibs.PluralNoun6 = noun6;
       newLibs.Adjective1 = adjective1;
       newLibs.Adjective2 = adjective2;
       newLibs.Adjective3 = adjective3;
--- AngryLibs/Models/ChooseLibs.cs
+++ AngryLibs/Models/ChooseLibs.cs
@@ -8,6 +8,7 @@ namespace AngryLibs.Models
     public string PluralNoun3 { get; set; }
     public string PluralNoun4 { get; set; }
     public string PluralNoun5 { get; set; }
+    public string PluralNoun6 { get; set; }
     public string Adjective1 { get; set; }
     public string Adjective2 { get; set; }
     public string Adjective3 { get; set; }
